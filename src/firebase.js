237fdffcddbcdaa467e6b2c6b41544b7d46e1f48
Viplav Jha <Viplavjha1993@gamil.com>
2021-02-05 22:40:45 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxCAuR4OeDqdJO-kFwHCE-5_VyWnLBh38",
+    authDomain: "auth-redux-c4966.firebaseapp.com",
+    projectId: "auth-redux-c4966",
+    storageBucket: "auth-redux-c4966.appspot.com",
+    messagingSenderId: "577771786635",
+    appId: "1:577771786635:web:7732867841adbab49a1f8e",
+    measurementId: "G-FHRDG2Z7C6"
+  };
